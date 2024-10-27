016fccab5dc219f5025821c52b8a00399a6d5f21
Nelia11 <levit.nelia@icloud.com>
2024-10-27 20:57:36 +0100

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Header from './components/navBar/NavBar'
+import Home from './pages/Home'
+
 function App() {
   return (
-    <>
-      <h1 className='text-xl font-bold text-red-500'>Hello TailwindCSS with React Vite</h1>
-    </>
+    <div className='flex justify-center bg-blue-800 h-[100rem]'>
+      <Header />
+      <Home />
+    </div>
   )
 }
 
---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import logo from '../../assets/logo.svg'
+
+const NavBar = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const menuItems: { id: string, label: string }[] =
+    [
+      { id: "00", label: "home" },
+      { id: "01", label: "destination" },
+      { id: "02", label: "crew" },
+      { id: "03", label: "technology" },
+    ];
+
+  return (
+    <div className="flex items-center justify-center fixed h-[10rem] w-full max-w-[1440px]">
+      <div className="flex items-center justify-between w-full pl-10">
+        <img src={logo} alt="logo" />
+        <div className='flex items-center relative'>
+          <div className="h-[.1rem] bg-white w-[30rem] absolute -left-1/2"
+            style={{ backgroundColor: 'rgba(255, 255, 255, 0.25)' }}
+          ></div>
+          <nav className="flex items-center h-[10rem]"
+            style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}
+          >
+            <div className="flex items-center justify-between list-none space-x-14 text-white uppercase mr-20 ml-40 text-[1.6rem]">
+              {menuItems.map((item, index) => (
+                <div
+                  key={item.id}
+                  className={`flex items-center h-[10rem] ${activeIndex === index && 'border-b-2 border-white'}`}
+                  onClick={() => setActiveIndex(index)}
+                >
+                  <div className="cursor-pointer">
+                    <span className="font-bold">{item.id} </span>{item.label}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
