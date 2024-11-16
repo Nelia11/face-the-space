c596c99576949256046814e34999f2b8c698c3a2
Nelia11 <levit.nelia@icloud.com>
2024-11-16 20:13:00 +0100

    Use Layout for Technology page

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -11,6 +11,7 @@ import spaceCapsulePortrait from '../assets/technology/image-space-capsule-portr
 import spaceportLandscape from '../assets/technology/image-spaceport-landscape.jpg';
 import spaceportPortrait from '../assets/technology/image-spaceport-portrait.jpg';
 import { Tech } from '../interfaces/SpaceTravelData';
+import Layout from '../components/layout/Layout';
 
 interface TechnologyProps {
   technologies: Tech[];
@@ -39,47 +40,19 @@ const Technology: FC<TechnologyProps> = ({ technologies }) => {
           landscapeImage: spaceCapsuleLandscape,
           portraitImage: spaceCapsulePortrait
         };
-      default:
-        return tech;
     }
   });
 
   const isDesktop = useMediaQuery({ minWidth: 1024 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
 
-  const selectedTech = mappedTechnologies[selectedTechnology];
+  const selectedTech = mappedTechnologies.find((_, index) => index === selectedTechnology);
 
   return (
-    <div
-      className="fixed inset-0 flex flex-col w-full text-white overflow-hidden"
-      style={{ height: '100vh' }}
-    >
-      <div
-        className="absolute inset-0 bg-cover bg-center z-0"
-        style={{
-          backgroundImage: `url(${
-            isDesktop ? backgroundDesktop : isTablet ? backgroundTablet : backgroundMobile
-          })`
-        }}
-      ></div>
-
-      <div
-        className="absolute text-gray-400 text-lg md:text-xl tracking-widest uppercase z-20"
-        style={{
-          top: '20%',
-          left: '10%',
-          transform: 'translateY(-50%)'
-        }}
-      >
-        03 <span className="text-white">Space Launch 101</span>
-      </div>
-
-      <div
-        className={`relative flex flex-col lg:flex-row items-center justify-between w-full h-full ${
-          isDesktop ? 'pt-36' : 'pt-64 md:pt-72'
-        } px-6 md:px-12 lg:px-32 space-y-6 lg:space-y-0 z-10`}
-      >
-        <div className="flex flex-row lg:flex-col items-center space-x-4 lg:space-x-0 lg:space-y-10 z-20">
+    <Layout bgMob={backgroundMobile} bgTablet={backgroundDesktop} bgDesktop={backgroundTablet}>
+      {/* Main Content */}
+      <div className="relative flex flex-col lg:flex-row items-center justify-between w-full h-full px-6 space-y-6 lg:space-y-0 gap-1">
+        {/* Sidebar with Buttons */}
+        <div className="flex flex-row lg:flex-col items-center space-x-4 lg:space-x-0 lg:space-y-10">
           {mappedTechnologies.map((_, index) => (
             <button
               key={index}
@@ -95,18 +68,22 @@ const Technology: FC<TechnologyProps> = ({ technologies }) => {
           ))}
         </div>
 
-        <div className="lg:flex-grow text-center lg:text-left max-w-lg space-y-4 lg:ml-8 z-20">
-          <h3 className="text-gray-500 uppercase text-sm tracking-widest">The Terminology...</h3>
+        {/* Technology Information */}
+        <div className="lg:flex-grow text-center lg:text-left max-w-lg space-y-4 lg:ml-8 m-10">
+          <p className="text-sm md:text-base lg:text-[2rem] tracking-widest text-gray-300 opacity-50 uppercase mt-1 mb-1 font-heebo">
+            The Terminology...
+          </p>
           <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl uppercase font-bold tracking-[0.15em]">
             {selectedTech?.name}
           </h1>
-          <p className="text-gray-400 text-sm md:text-base lg:text-lg leading-relaxed">
+          <p className="text-gray-400 text-sm md:text-base lg:text-lg leading-relaxed text-justify">
             {selectedTech?.description}
           </p>
         </div>
 
-        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end z-20">
-          <LazyLoad height={600} offset={100}>
+        {/* Technology Image aligned to the right */}
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+          <LazyLoad>
             <img
               src={isDesktop ? selectedTech?.portraitImage : selectedTech?.landscapeImage}
               alt={selectedTech?.name}
@@ -115,7 +92,7 @@ const Technology: FC<TechnologyProps> = ({ technologies }) => {
           </LazyLoad>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
