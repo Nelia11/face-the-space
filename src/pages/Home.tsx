d59ce346b2d3b0db7270e5711730d55597d78ee1
Yağmur Nielsen <yagmureva4@gmail.com>
2024-11-18 20:26:10 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,8 +10,8 @@ const Home = () => {
 
   return (
     <Layout bgMob={backgroundMobile} bgTablet={backgroundTablet} bgDesktop={backgroundDesktop}>
-      <div className="flex flex-col items-center lg:items-start space-y-4 max-w-md text-center lg:text-left px-5 lg:px-10 lg:pl-20 lg:mt-24 min-h-screen">
-        {/* Ensured full-screen height for better centering */}
+      {/* Increased the margin-top to move the text further down */}
+      <div className="flex flex-col space-y-4 max-w-md lg:max-w-md text-center md:text-center lg:text-left px-5 lg:px-10 lg:pl-20 mt-6 md:mt-8">
         <h2 className="text-lg md:text-xl tracking-widest text-gray-300 uppercase">
           So, you want to travel to
         </h2>
@@ -22,8 +22,8 @@ const Home = () => {
           truly out-of-this-world experience!
         </p>
       </div>
-      <div className="flex items-center justify-center lg:justify-start mt-16 md:mt-24 lg:mt-32 lg:ml-20 lg:mr-10">
-        {/* Used lg:justify-start for proper alignment on larger screens */}
+      {/* Increased the margin-top to move the button further down */}
+      <div className="flex items-center justify-center mt-14 md:mt-28 lg:mt-24 lg:ml-20 lg:mr-10">
         <div
           onClick={handleExploreClick}
           className="bg-white text-black w-32 h-32 md:w-40 md:h-40 lg:w-60 lg:h-60 rounded-full flex items-center justify-center cursor-pointer shadow-lg transition-transform transform hover:scale-105"
